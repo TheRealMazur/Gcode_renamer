bc02d8eca3fe8b7d2fad67106a28ba8023cd9c55
krzmazur1 <krzmazur1@gmail.com>
2020-04-18 15:18:21 +0200

    Make build script more universal

--- build.ps1
+++ build.ps1
@@ -1,7 +1,39 @@
-param([string]$BuildType="Release")
+<#
+    .SYNOPSIS
+        Build script for the project.
+#>
+[CmdletBinding(ConfirmImpact = 'Medium', DefaultParameterSetName = 'DefParamSet')]
+param(
+    [Parameter(ParameterSetName = 'DefParamSet', Mandatory)]
+        [Alias("c")]
+        [ValidateSet("Debug", "Release")]
+        [string]$config, # Build config to run
+    [Parameter(ParameterSetName = 'DefParamSet',Mandatory)]
+        [Alias("g")]
+        [ValidateSet("Ninja", "Visual Studio 15 2017")]
+        [string]$generator, # Generator to use
+    [Parameter(ParameterSetName = 'DefParamSet')]
+        [Alias("h")]
+        [ValidateSet('x64','x86')]
+        [string]$hostarch, # Host architecture for the toolset
+    [Parameter(ParameterSetName = 'DefParamSet')]
+        [Alias("a")]
+        [ValidateSet('x64','x86')]
+        [string]$arch # Architecture to use
+)
+
+$config = $config.ToUpper()
+
+if($hostarch){
+    $hostparameter = "-Thost=$hostarch"
+}
+
+if($arch){
+    $archparameter = "-A$arch"
+}
 
 cmake -E make_directory $PSScriptRoot/build
 Set-Location $PSScriptRoot/build
-cmake -G "Visual Studio 15 2017" -A x64 -T host=x86 .. "-DCMAKE_BUILD_TYPE=$BuildType"
-cmake --build . --config $BuildType
+cmake -G $generator $hostparameter $archparameter .. "-DCMAKE_BUILD_TYPE=$config"
+cmake --build . --config $config 
 Set-Location $PSScriptRoot
